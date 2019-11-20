813f44109dd801dcdce796699d407c376a0bfe35
aliakseis <aliaksei.sanko@gmail.com>
2019-11-20 11:19:37 +0300

    missing stuff added

---
+++ github.com/gogo/protobuf/gogoproto/gogo.pb.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <google/protobuf/port_def.inc>
+
+static bool is_descriptor_table_github_2ecom_2fgogo_2fprotobuf_2fgogoproto_2fgogo_2eproto_initialized = true;
+
+const ::PROTOBUF_NAMESPACE_ID::internal::DescriptorTable descriptor_table_github_2ecom_2fgogo_2fprotobuf_2fgogoproto_2fgogo_2eproto
+{ &is_descriptor_table_github_2ecom_2fgogo_2fprotobuf_2fgogoproto_2fgogo_2eproto_initialized };
+
+#include <google/protobuf/port_undef.inc>
